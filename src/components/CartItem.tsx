287d83362fcf2d5e80f34aa00a6c2b57023c86fa
Karunika <karunikatrip@gmail.com>
2022-07-30 01:38:42 +0200

    fix: icons

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
-import { editCart, save } from '../feature/cartSlice';
+import { editCart, removeCart, save } from '../feature/cartSlice';
 import { I_Cart } from '../global';
 
 import { useUnloadCautionPrompt } from '../app/hooks';
@@ -11,9 +11,9 @@ import moment from 'moment';
 
 import {
     BiGridVertical,
-    // BiRename,
+    BiRename,
     BiCheck,
-    // BiTrash,
+    BiTrash,
     BiX,
 } from 'react-icons/bi';
 
@@ -36,29 +36,36 @@ const CartItem: FC<props> = ({
     const [editableName, setEditableName] = useState(name);
     const [editing, setEditing] = useState(false);
 
-    // const deleteHandler = () => {
-    //     dispatch(removeCart(_id));
-    //     dispatch(save());
-    // };
+    const deleteHandler = () => {
+        dispatch(removeCart(_id));
+        dispatch(save());
+    };
+
     const editClickHandler = () => {
         setEditing(prev => !prev);
     };
+
     const saveClickHandler = () => {
         editClickHandler();
         dispatch(editCart([_id, editableName]));
         dispatch(save());
     };
+
     const discardClickHandler = () => {
         setEditableName(name);
         editClickHandler();
     };
+
     const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.value.length < 32) setEditableName(e.target.value);
     };
+
     useEffect(() => {
         if (editing && inputRef && inputRef.current) inputRef.current.focus();
     }, [editing]);
+
     useUnloadCautionPrompt(editing);
+
     return (
         <>
             <DndTable.Data>
@@ -122,34 +129,28 @@ const CartItem: FC<props> = ({
             </DndTable.Data>
 
             <DndTable.Data>
-                <Tooltip content='save'>
-                    <BiCheck className='green' onClick={saveClickHandler} />
-                </Tooltip>
+                {editing ? (
+                    <Tooltip content='save'>
+                        <BiCheck className='green' onClick={saveClickHandler} />
+                    </Tooltip>
+                ) : (
+                    <Tooltip content='rename cart'>
+                        <BiRename onClick={editClickHandler} />
+                    </Tooltip>
+                )}
             </DndTable.Data>
 
             <DndTable.Data>
-                <Tooltip content='discard'>
-                    <BiX className='red' onClick={discardClickHandler} />
-                </Tooltip>
+                {editing ? (
+                    <Tooltip content='discard'>
+                        <BiX className='red' onClick={discardClickHandler} />
+                    </Tooltip>
+                ) : (
+                    <Tooltip content='delete'>
+                        <BiTrash className='red' onClick={deleteHandler} />
+                    </Tooltip>
+                )}
             </DndTable.Data>
-            {/* {editing ? (
-                <>
-                </>
-            ) : (
-                <>
-                    <DndTable.Data>
-                        <Tooltip content='rename cart'>
-                            <BiRename onClick={editClickHandler} />
-                        </Tooltip>
-                    </DndTable.Data>
-
-                    <DndTable.Data>
-                        <Tooltip content='delete'>
-                            <BiTrash className='red' onClick={deleteHandler} />
-                        </Tooltip>
-                    </DndTable.Data>
-                </>
-            )} */}
         </>
     );
 };
